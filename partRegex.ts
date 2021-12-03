a1cc2faffcc6c78236949f0d290113802dd2e701
Romuald Brillout <git@brillout.com>
2021-12-03 16:04:05 +0100

    feat: support string partials

--- partRegex.ts
+++ partRegex.ts
@@ -4,18 +4,27 @@ import { slice } from './utils/slice'
 export { partRegex }
 export default partRegex
 
-function partRegex(parts: TemplateStringsArray, ...variables: RegExp[]): RegExp {
+function partRegex(parts: TemplateStringsArray, ...variables: (RegExp | unknown)[]): RegExp {
   assert(parts.length === variables.length + 1)
   let str = ''
   for (let i = 0; i < variables.length; i++) {
     const variable = variables[i]
-    assert(variable.constructor === RegExp)
-    str += escapeRegex(parts[i]) + slice(variable.toString(), 1, -1)
+    str += escapeString(parts[i])
+    if (isRegex(variable)) {
+      str += slice(variable.toString(), 1, -1)
+    } else {
+      str += escapeString(String(variable))
+    }
   }
-  str += escapeRegex(parts[parts.length - 1])
+  str += escapeString(parts[parts.length - 1])
   return new RegExp(str)
 }
 
-function escapeRegex(str: string) {
+function escapeString(str: string) {
   return str.replace(/[-\/\\^$*+?.()|[\]{}]/g, '\\$&')
+}
+
+// https://github.com/sindresorhus/is-regexp/blob/e38696a04645a94a0c1db00ecf8a28822a33b5f6/index.js
+function isRegex(value: unknown): value is RegExp {
+  return toString.call(value) === '[object RegExp]'
 }
--- test.mjs
+++ test.mjs
@@ -1,15 +1,8 @@
-Create a RegExp by defining some parts with strings and other parts with RegExp.
+import { partRegex } from './dist/partRegex.js'
 
-```js
-import partRegex from 'part-regex' // npm install part-regex
-
-// Only match strings `/product/[0-9]+/edit`
 const productEditPageUrl = partRegex`/product/${/[0-9]+/}/edit`
 console.log(productEditPageUrl.test('/product/42/edit')) // Prints `true`
 
-// Also supports a mix of String and RegEx variables.
-// == Note how the first part is a RegEx and the second part is a string.
 const userPageUrl = (isSettingsPage) => partRegex`/user/${/[a-zA-Z]+/}${isSettingsPage ? '/settings' : ''}`
 console.log(userPageUrl(true).test('/user/brillout/settings')) // Prints `true`
 console.log(userPageUrl(false).test('/user/turing')) // Prints `true`
-```
